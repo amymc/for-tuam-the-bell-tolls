977a36aba77969902ec632607f0730b55f0dd9a0
amymc <amy.mccarthy2@mail.dcu.ie>
2021-09-04 23:32:59 +0100

    update vite base config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 
 export default defineConfig({
-  base: "https://www.amymccarthy.co/for-tuam-the-bell-tolls/",
+  base: "/for-whom-the-bell-tolls/",
   plugins: [preact()],
   esbuild: {
     jsxFactory: `jsx`,
