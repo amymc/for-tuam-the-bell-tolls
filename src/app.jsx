69cbfcd4ed8c418a3ee5e7503929d2135ed284de
amymc <amy.mccarthy2@mail.dcu.ie>
2021-09-05 23:20:37 +0100

    clear timeout

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState, useRef } from "preact/hooks";
-// import * as Promise from "bluebird";
 import RemembranceCard from "./RemembranceCard";
 import RewindIcon from "./RewindIcon";
 import SkipStartIcon from "./SkipStartIcon";
@@ -8,11 +7,6 @@ import separator from "./assets/separator.svg";
 import children from "./children.json";
 import mp3 from "./assets/angelus-bell-7.mp3";
 import "./app.css";
-
-// Promise.config({
-//   // Enable cancellation
-//   cancellation: true,
-// });
 
 const stages = {
   start: "start",
@@ -25,179 +19,26 @@ export function App(props) {
   const [child, setChild] = useState(null);
   const [stage, setStage] = useState(stages.start);
   const [stackedNum, setStackedNum] = useState(null);
-
-  let shouldCancel = useRef(false);
+  let timeout = useRef(null);
 
   useEffect(() => {
     window.speechSynthesis.onvoiceschanged = () => setIsReady(true);
+
+    return () => clearTimeout(timeout.current);
   }, []);
 
   let audioPromise = Promise.resolve();
-  // let audioPromise = new Promise((resolve, reject, onCancel) => {
-  //   // const to = setTimeout(() => resolve(), 2000);
-  //   // setTimeout(resolve, 1000);
-  //   // resolve();
-  //   onCancel(() => {
-  //     console.log("onCancel called");
-  //     // clearTimeout(to);
-  //     reject(new Error("cancel")); // no error throwed. With resolve() in this place it also hangs on await
-  //   });
-  // });
-
-  // const newAudio = async (child, signal) => {
-  //   playAudio(child.name);
-  //   setChild(child);
-  //   return new Promise(function (resolve) {
-  //     if (!signal.aborted) {
-  //       setTimeout(resolve, 3000);
-  //     }
-  //   });
-  // };
 
   const onClick = async (children) => {
-    console.log("click!!!");
-    // if (shouldCancel.current) return;
-
-    // Promise.each(children, function (child) {
-    //   // playAudio(child.name);
-    //   // setChild(child);
-    //   testP = new Promise(function (resolve, reject, onCancel) {
-    //     // setTimeout(resolve, 3000);
-    //     console.log("shouldCancel", shouldCancel.current);
-    //     if (!shouldCancel.current) {
-    //       playAudio(child.name);
-    //       setChild(child);
-    //       setTimeout(resolve, 3000);
-    //     }
-    //     onCancel(() => {
-    //       console.log("onCancel called");
-    //       // clearTimeout(to);
-    //       reject(new Error("cancel")); // no error throwed. With resolve() in this place it also hangs on await
-    //     });
-    //   });
-    //   // return new Promise(function (resolve, reject, onCancel) {
-    //   //   playAudio(child.name);
-    //   //   setChild(child);
-    //   //   setTimeout(resolve, 3000);
-    //   //   onCancel(() => {
-    //   //     console.log("onCancel called");
-    //   //     // clearTimeout(to);
-    //   //     reject(new Error("cancel")); // no error throwed. With resolve() in this place it also hangs on await
-    //   //   });
-    //   // });
-
-    //   return testP;
-    // });
-
     for (let child of children) {
-      console.log("wtf", child);
-      if (shouldCancel.current) {
-        console.log("break", child);
-
-        break;
-      }
       audioPromise = audioPromise.then(() => {
-        if (shouldCancel.current) return false;
-        // if (!shouldCancel.current) {
-        console.log("child", child);
-        console.log("child", children);
-
-        // console.log("child ind", index);
-
         playAudio(child.name);
         setChild(child);
-
-        // setStackedNum((stackedNum) =>
-        //   stackedNum < 10 ? stackedNum++ : stackedNum
-        // );
-        // if (!signal.aborted) {
-        // testP = new Promise(function (resolve, reject, onCancel) {
-        //   setTimeout(resolve, 3000);
-        //   onCancel(() => {
-        //     console.log("onCancel called wewerwuer");
-        //     // clearTimeout(to);
-        //     reject(new Error("cancel")); // no error throwed. With resolve() in this place it also hangs on await
-        //   });
         return new Promise(function (resolve, reject, onCancel) {
-          // playAudio(child.name);
-          // setChild(child);
-          if (shouldCancel.current) {
-            reject(new Error("cancel"));
-          }
-          setTimeout(resolve, 3000);
-
-          // setTimeout(resolve, 3000);
-          // onCancel(() => {
-          //   console.log("onCancel called wewerwuer");
-          //   // clearTimeout(to);
-          //   reject(new Error("cancel")); // no error throwed. With resolve() in this place it also hangs on await
-          // });
-          // timeout(resolve, 3000, signal);
-
-          // signal.addEventListener("abort", listener);
-          // signal.addEventListener("abort", () => {
-          //   ComponentSelector.log("aborting ", signal);
-          //   window.clearTimeout(timeout);
-          //   reject(new DOMException("Aborted", "AbortError"));
-          // });
-          // }
+          timeout.current = setTimeout(resolve, 3000);
         });
-        // }
       });
     }
-
-    // children.map(async (child, index) => {
-    //   // audioPromise.cancel();
-    //   console.log("child outer", child);
-    //   if (shouldCancel.current) return;
-    //   audioPromise = audioPromise.then(() => {
-    //     if (shouldCancel.current) return;
-    //     // if (!shouldCancel.current) {
-    //     console.log("child", child);
-    //     console.log("child", children);
-
-    //     console.log("child ind", index);
-
-    //     playAudio(child.name);
-    //     setChild(child);
-
-    //     // setStackedNum((stackedNum) =>
-    //     //   stackedNum < 10 ? stackedNum++ : stackedNum
-    //     // );
-    //     // if (!signal.aborted) {
-    //     // testP = new Promise(function (resolve, reject, onCancel) {
-    //     //   setTimeout(resolve, 3000);
-    //     //   onCancel(() => {
-    //     //     console.log("onCancel called wewerwuer");
-    //     //     // clearTimeout(to);
-    //     //     reject(new Error("cancel")); // no error throwed. With resolve() in this place it also hangs on await
-    //     //   });
-    //     return new Promise(function (resolve, reject, onCancel) {
-    //       // playAudio(child.name);
-    //       // setChild(child);
-    //       if (shouldCancel.current) return;
-
-    //       setTimeout(resolve, 3000);
-
-    //       // setTimeout(resolve, 3000);
-    //       // onCancel(() => {
-    //       //   console.log("onCancel called wewerwuer");
-    //       //   // clearTimeout(to);
-    //       //   reject(new Error("cancel")); // no error throwed. With resolve() in this place it also hangs on await
-    //       // });
-    //       // timeout(resolve, 3000, signal);
-
-    //       // signal.addEventListener("abort", listener);
-    //       // signal.addEventListener("abort", () => {
-    //       //   ComponentSelector.log("aborting ", signal);
-    //       //   window.clearTimeout(timeout);
-    //       //   reject(new DOMException("Aborted", "AbortError"));
-    //       // });
-    //       // }
-    //     });
-    //     // }
-    //   });
-    // });
   };
 
   function play(url) {
@@ -218,39 +59,27 @@ export function App(props) {
     utterance.rate = 0.9;
     const voices = window.speechSynthesis.getVoices();
     utterance.voice = voices.find((voice) => voice.name == "Moira");
-    // audioElement.play();
     play(mp3);
-    // if (shouldCancel.current) return;
-
     window.speechSynthesis.speak(utterance);
   }
 
   const start = (e) => {
-    // e.stopPropagation();
-    shouldCancel.current = false;
     onClick(children);
     setStage(stages.card);
   };
-  const pause = () => {
-    shouldCancel.current = true;
-  };
+
+  const pause = () => clearTimeout(timeout.current);
 
   const resume = () => {
-    shouldCancel.current = false;
     setStage(stages.card);
     const index = children.findIndex((item) => item === child);
-    console.log("index", index);
-    console.log("children", children);
     onClick(children.slice(index + 1));
   };
 
-  const skipToStart = () => {
-    shouldCancel.current = true;
-    setStage(stages.start);
-  };
+  const skipToStart = () => setStage(stages.start);
 
   const skipToEnd = () => {
-    shouldCancel.current = true;
+    clearTimeout(timeout.current);
     setStage(stages.end);
   };
 
@@ -267,13 +96,14 @@ export function App(props) {
               1960, a child died on average every two weeks at this home. <br />
               <br />
               In total 978 children died there in the "care" of the nuns during
-              that time. These are their names.
+              that time. Of those, the identity of almost 800 are known. These
+              are their names.
+              <button class="start-btn" onClick={start} disabled={!isReady}>
+                Toll the bell
+              </button>
               <img class="separator" src={separator} />
             </div>
           </div>
-          <button class="start-btn" onClick={start} disabled={!isReady}>
-            Toll the bell
-          </button>
         </>
       )}
 
